3e2c455f61be1e96541b3f37e50ce86e48cf4956
Jonathan Gerard <jonathangerard480@gmail.com>
2025-06-05 16:49:27 +0530

    initial commit

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const print = std.debug.print;
 const expect = std.testing.expect;
 
 pub fn main() !void {
-    print("This is a Zig program!\n", .{});
+    print("This is a simple Zig program!\n", .{});
 }
 
 test "simple" {
